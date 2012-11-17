086c8686367e299c90487f45b2d08b941a08ab3d
Daniel Friedman <danielnfriedman@gmail.com>
2012-11-17 15:30:08 +1100

    Cleanup and remove unnecessary boilerplate

--- app.js
+++ app.js
@@ -1,35 +1,76 @@
-
 /**
  * Module dependencies.
  */
 
 var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
   , http = require('http')
-  , path = require('path');
+  , ninjaBlocks = require('ninja-blocks');
 
 var app = express();
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
   app.use(express.bodyParser());
-  app.use(express.methodOverride());
   app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler());
+// Create a new Ninja Block app. We'll use the user access token for this
+var ninja = ninjaBlocks.app({user_access_token:'USER_ACCESS_TOKEN'});
+
+// Map of the state to short name of the relay
+var states = {
+  OKAY:'Okay',
+  BUILD:'Build',
+  FAIL:'Fail'
+};
+
+// To be map of the relay shortname to device GUID
+var relays={};
+
+// On startup, fetch the user's devices
+ninja.devices('relay',function(err,devices) {
+
+  if (err) throw err.error;
+
+  // Build the map of shortname to device GUID
+  Object.keys(devices).forEach(function(guid) {
+    relays[devices[guid].shortName] = guid
+  });
 });
 
-app.get('/', routes.index);
-app.post('/status',routes.status);
+// GitHub post commit hook
+app.post('/building',function(req,res) {
+
+  console.log('Building')
+  ninja.device(relays[states.FAIL]).actuate('0')
+  ninja.device(relays[states.OKAY]).actuate('0')
+  ninja.device(relays[states.BUILD]).actuate('1')
+  res.end();
+});
+
+// Travis-CI build status hook
+app.post('/status',function(req,res) {
+
+  // Parse the response from Travis-CI
+  var payload = JSON.parse(req.body.payload);
+
+  // Turn off the building relay
+  ninja.device(relays[states.BUILD]).actuate('0')
+
+  if (payload.result==1) {
+    // Turn on the failed relay
+    console.log('Build Failed');
+    ninja.device(relays[states.FAIL]).actuate('1')
+  } else {
+    // Turn on the succeeded relay
+    console.log('Build Succeeded');
+    ninja.device(relays[states.OKAY]).actuate('1')
+  }
+  // End the response to travis
+  res.end();
+});
 
 http.createServer(app).listen(app.get('port'), function(){
-    console.log("Express server listening on port " + app.get('port'));
+  console.log("CI alert app listening on port " + app.get('port'));
 });
+
